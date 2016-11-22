d8ee94cc5f845cf57fc36e7b66bcbff140a324a2
Olivier Nourry <onourry@hotmail.fr>
2016-11-21 21:10:11 -0500

    Added Casts,Change from Generic to Object,Added exception handling,fixed bugs

--- LinearHash.java
+++ LinearHash.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class LinearHash<Key,Value>{
+public class LinearHash{
 	
 	private int nbrElements;
 	private int sizeHash;
@@ -9,16 +9,16 @@ public class LinearHash<Key,Value>{
 	private final double LOAD_FACTOR_TOO_BIG = 0.75;
 	private final double LOAD_FACTOR_TOO_SMALL = 0.20;
 	
-	private Key[] keys;
-	private Value[] values;
+	private Object[] keys;
+	private Object[] values;
 	
 	//Default constructor
 	public LinearHash(){
 		this.sizeHash = this.DEFAULT_SIZE;
 		this.nbrElements = 0;
 		
-		this.keys = (Key[]) new Object[this.DEFAULT_SIZE];
-		this.values = (Value[]) new Object[this.DEFAULT_SIZE];
+		this.keys = new Object [this.DEFAULT_SIZE];
+		this.values =  new Object[this.DEFAULT_SIZE];
 	}
 	
 	//Constructor taking a size as argument
@@ -26,8 +26,8 @@ public class LinearHash<Key,Value>{
 		this.sizeHash = size;
 		this.nbrElements = 0;
 		
-		this.keys = (Key[]) new Object[this.sizeHash];
-		this.values = (Value[]) new Object[this.sizeHash];
+		this.keys =  new Object[this.sizeHash];
+		this.values = new Object[this.sizeHash];
 	}
 	
 	public int size(){
@@ -35,16 +35,25 @@ public class LinearHash<Key,Value>{
 	}
 	
 	//Put a value and a key in the hash table
-	public void put(Key key, Value value) throws Exception{
+	public void put(Object key, Object value){
 		//Invalid key
-		if(key == null)
-			throw new Exception("Invalid Key");
+		try{
+			if(key == null)
+				throw new Exception("Invalid Key");
+		} catch(Exception e){
+			e.printStackTrace();;
+		}
 		
 		int index = hashKey(key);
 		
 		//Key valid but empty value
-		if(value == null){
-			throw new Exception("Invalid Value");
+		try{
+			if(value == null){
+				throw new Exception("Invalid Value");
+			}
+		} 
+		catch(Exception e){
+			e.printStackTrace();
 		}
 		
 		//If we exceed the 0.75 load, double the size
@@ -60,7 +69,7 @@ public class LinearHash<Key,Value>{
 			for(newIndex = index; keys[newIndex] != null; newIndex++){
 				
 				//Key already in hashtable, update value
-				if(keys[newIndex].equals(key)){
+				if(keys[newIndex] == key){
 					values[newIndex] = value;
 					return;
 				}
@@ -80,25 +89,34 @@ public class LinearHash<Key,Value>{
 		
 	}
 	
-	public boolean contains (Key key) throws Exception{
-		if(key == null)
-			throw new Exception("Invalid Key");
+	public boolean contains (Object key){
+		try{
+			if(key == null)
+				throw new Exception("Invalid Key");
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 		return get(key) != null;
 	}
 	
 	//Get the corresponding value of the key given in parameter
-	public Value get(Key key) throws Exception{
-		if(key == null)
-			throw new Exception("Invalid Key");
+	public Object get(Object key){
+		try{
+			if(key == null)
+				throw new Exception("Invalid Key");
 			
+		} catch (Exception e){
+			e.printStackTrace();
+		}	
 		for(int i = hashKey(key); keys[i] != null; i++){
 			
 			
 			//There has been something there before but it was deleted, to avoid issues with the .equals method
-			if(keys[i] == ("-1"))
+			if(keys[i].toString() == ("-1"))
 				continue;
 			
-			if(keys[i].equals(key)){
+			if(keys[i] == key){
 				return values[i];
 			}
 			
@@ -112,19 +130,22 @@ public class LinearHash<Key,Value>{
 	}
 	
 	//Remove a key (Set to -1) from the hashtable and set the value to null
-	public void remove(Key key) throws Exception{
-		
-		if(key == null)
-			throw new Exception("Invalid Key");
-		
+	public void remove(Object key){
+		try{
+			if(key == null)
+				throw new Exception("Invalid Key");
+			
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 		if (!contains(key))
 			return;
 		
 		for(int i = hashKey(key); keys[i] != null; i++){
-			
+			//-----------------------------------------------------------------------replace by empty object??
 			if(keys[i].equals(key)){
-				keys[i] = (Key)("-1");
-				values[i] = (Value)"-1";
+				keys[i] = ("-1");
+				values[i] = ("-1");
 			}
 			
 			//Return to beginning of array and keep searching if last index has been reached
@@ -140,22 +161,22 @@ public class LinearHash<Key,Value>{
 	}
 	
 	//Get hash code for the key
-	private int hashKey(Key key){
+	private int hashKey(Object key){
 		return (key.hashCode() % this.sizeHash);
 	}
 	
-	public Set<Key> keySet(){
 		
-		Set<Key> keySet = new HashSet<Key>(Arrays.asList(keys));
+	public Set<Object> keySet(){
+		Set<Object> keySet = new HashSet<Object>(Arrays.asList(keys));
 		
 		return keySet;
 	}
 	
 	//Resize the hashtable
 	private void resize(int sizeHash){
-		LinearHash<Key,Value> newHash = new LinearHash<Key,Value>(sizeHash);
+		LinearHash newHash = new LinearHash(sizeHash);
 		
-		for(Key key : this.keySet()){
+		for(Object key : this.keySet()){
 			try {
 				newHash.put(key, this.get(key));
 			} catch (Exception e) {
--- SmartULS.java
+++ SmartULS.java
@@ -1,158 +1,255 @@
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 //Trees,Hash Tables, AVL tree, Binary Tree, sequence
 
 public class SmartULS {
-	
+
 	private int tresholdULS = 1000;
 	private Object structure;
-	
-	
-	public SmartULS(){
-		(LinearHash)this.structure = new LinearHash();
+	private int size;
+
+	public SmartULS() {
+		this.structure = new LinearHash();
+		this.size = ((LinearHash) this.structure).size();
 	}
 
-	
-	public SmartULS(long size){
-		if(size >= this.tresholdULS){
-			(Hashtable)this.structure = new Hashtable(size);
+	public SmartULS(int size) {
+		if (size >= this.tresholdULS) {
+			this.structure = new Hashtable(size);
+			this.size = ((Hashtable) this.structure).size();
 		}
-		
-		else{
-			(LinearHash)this.structure = new LinearHash(size);
+
+		else {
+			this.structure = new LinearHash(size);
+			this.size = ((LinearHash) this.structure).size();
 		}
 	}
-	
-	public void setSmartThresholdULS(long size){
+
+	public void setSmartThresholdULS(int size) {
 		this.tresholdULS = size;
 	}
-	
+
 	/**
 	 * Randomly generates a new non-existing key of 8 digits
+	 * 
 	 * @return (long) key of 8 digits
 	 */
-	public long generate(){
-		
+	public long generate() {
+
 		long randomKey = ThreadLocalRandom.current().nextLong(99999999);
-		
-		try {
-			
-			if(this.structure.contains(randomKey)){
-				while (this.structure.contains(randomKey)){
-					
+		if (this.structure instanceof LinearHash) {
+			if (((LinearHash) this.structure).contains(randomKey)) {
+				while (((LinearHash) this.structure).contains(randomKey)) {
+
 					randomKey = ThreadLocalRandom.current().nextLong(99999999);
 				}
 			}
-			
-			
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		} else if (this.structure instanceof Hashtable) {
+			if (((Hashtable) this.structure).contains(randomKey)) {
+				while (((Hashtable) this.structure).contains(randomKey)) {
+
+					randomKey = ThreadLocalRandom.current().nextLong(99999999);
+				}
+			}
 		}
-		
+
+		else {
+			try {
+				throw new Exception("Unrecognized structure");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
 		return randomKey;
 	}
-	
+
 	/**
 	 * return all keys in SmartULS as a sorted sequence
-	 * @param uls SmartULS object
+	 * 
+	 * @param uls
+	 *            SmartULS object
 	 */
-	public Set allKeys(SmartULS uls){
-		Set<Key> keys;
-		if(this.structure instanceof Hashtable){
-			keys = (Hashtable)this.structure.keySet()
+	public Set allKeys(SmartULS uls) {
+		Set<Object> keys = new HashSet<Object>();
+		
+		if (this.structure instanceof Hashtable) {
+			keys = ((Hashtable) this.structure).keySet();
+		}
+
+		else if (this.structure instanceof LinearHash) {
+			keys = ((LinearHash) this.structure).keySet();
+			
+		} else {
+			try {
+				throw new Exception("Unrecognized structure");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
-		else if(this.structure instanceof LinearHash){
-			keys = (LinearHash)this.structure.keySet();
-		}
-		
-		
+		return keys;
 	}
-	
+
 	/**
 	 * add an entry for the given key and value
-	 * @param uls	SmartULS object
-	 * @param key	(long) key to the value 
-	 * @param value	(Object) object to be added with the specified key
+	 * 
+	 * @param uls
+	 *            SmartULS object
+	 * @param key
+	 *            (long) key to the value
+	 * @param value
+	 *            (Object) object to be added with the specified key
 	 */
-	public void add(SmartULS uls,long key, Object value){
-		try{
-			this.structure.put(key,value);
-			
-			if(this.structure instanceof LinearHash && this.structure.size() >= this.tresholdULS){
-				this.shift();
+	public void add(SmartULS uls, long key, Object value) {
+
+		if (this.structure instanceof LinearHash) {
+			((LinearHash) this.structure).put(key, value);
+
+			if (((LinearHash) this.structure).size() >= this.tresholdULS) {
+				this.shift(this.structure);
 			}
-		} 
-		
-		catch (Exception e) {
-			e.printStackTrace();
+		} else if (this.structure instanceof Hashtable) {
+			((Hashtable) this.structure).put(key, value);
 		}
-		
-	}
-	
-	public void remove(SmartULS, long key, Object value){
-		
-		try{
-			this.structure.remove(key);
-			
-			if(this.structure instanceof Hashtable && this.structure.size() < this.tresholdULS){
-				this.shift();
+
+		else {
+			try {
+				throw new Exception("Unrecognized Structure");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} 
-		
-		catch (Exception e) {
-			e.printStackTrace();
 		}
+
 	}
-	
-	
-	public Object getValues(SmartULS uls, long key){
-		return this.structure.get(key);
-	}
-	
-	
-	public Key nextKey(SmartULS uls, long key){
+
+	public void remove(SmartULS uls, long key, Object value) {
 		
-		Set<Key> myKeys = this.structure.keySet();
-		
-		Iterator<Key> iterator = myKeys.iterator();
-		
-		//Check with compiler how to get key
-	}
-	
-	public void prevKey(SmartULS uls, long key){
-		
-	}
-	
-	public void rangeKey(long key1, long key2){
-		
-	}
-	
-	private Object shift(Object structure){
-		
-		//Object newStructure;
-		
-		if(structure instanceof Hashtable){
+		if (this.structure instanceof LinearHash){
+			((LinearHash) this.structure).remove(key);
 			
-			LinearHash newStructure = new LinearHash(structure.size());
-			
-			//Copy everything
-			
-			return newStructure
+			if(((LinearHash) this.structure).size() < this.tresholdULS){
+				this.shift(this.structure);
+			}
+		}
+		else if(this.structure instanceof Hashtable){
+			((Hashtable) this.structure).remove(key);
 		}
 		
-		else if(structure instanceof LinearHash){
-			
-			Hashtable newStructure = new Hashtable(structure.size());
-			
-			//Copy everything
-			
-			return newStructure
+	}
+
+	public Object getValues(SmartULS uls, long key) {
+		Object value = new Object();
+		
+		if (this.structure instanceof LinearHash){
+			value = ((LinearHash) this.structure).get(key);
+		}
+		else if (this.structure instanceof Hashtable){
+			value = ((Hashtable) this.structure).get(key);
+		}
+		else{
+			try{
+				throw new Exception("Unrecognized structure");
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		return value;
+		
+	}
+
+	public Object nextKey(SmartULS uls, long key) {
+		
+		Set<Object> myKeys = new HashSet<Object>();
+		
+		if (this.structure instanceof LinearHash){
+			myKeys = ((LinearHash) this.structure).keySet();
+		}
+		else if (this.structure instanceof Hashtable){
+			myKeys = ((Hashtable) this.structure).keySet();
 		}
 		
 		else{
+			try{	
+				throw new Exception("Unrecognized structure");
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		Iterator<Object> iterator = myKeys.iterator();
+		
+		while(iterator.hasNext()){
+			if((long)iterator.next() == key){
+				iterator = (Iterator<Object>) iterator.next() ;
+				return (long)iterator.next();
+			}
+			
+			iterator = (Iterator<Object>)iterator.next();
+		}
+		
+
+		//Couldnt find the key
+		return null;
+	}
+
+	public void prevKey(SmartULS uls, long key) {
+		
+
+		Set<Object> myKeys = new HashSet<Object>();
+		
+		if (this.structure instanceof LinearHash){
+			myKeys = ((LinearHash) this.structure).keySet();
+		}
+		else if (this.structure instanceof Hashtable){
+			myKeys = ((Hashtable) this.structure).keySet();
+		}
+		
+		else{
+			try{	
+				throw new Exception("Unrecognized structure");
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		//How to get previous key???
+		
+		
+	}
+
+	public void rangeKey(long key1, long key2) {
+
+	}
+
+	private Object shift(Object structure) {
+
+		// Object newStructure;
+
+		if (this.structure instanceof Hashtable) {
+
+			LinearHash newStructure = new LinearHash(this.structure.size());
+
+			// Copy everything
+
+			return newStructure;
+		}
+
+		else if (this.structure instanceof LinearHash) {
+
+			Hashtable newStructure = new Hashtable(this.structure.size());
+
+			// Copy everything
+
+			return newStructure;
+		}
+
+		else {
 			throw new Exception("Unrecognized structure");
 		}
-		//return newStructure
+		// return newStructure
 	}
 }
